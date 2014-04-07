4243e65fbc5d2b76f2a644d0794657dfdef1018b
yihua.huang <code4crafter@gmail.com>
2014-04-07 22:52:28 +0800

    add grunt for book build

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+var path = require("path");
+
+module.exports = function (grunt) {
+    grunt.loadNpmTasks('grunt-gitbook');
+    grunt.loadNpmTasks('grunt-gh-pages');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+
+    grunt.initConfig({
+        'gitbook': {
+            development: {
+                output: path.join(__dirname, ".grunt/gitbook"),
+                input: "./",
+                title: "Netty-learning",
+                description: "",
+                github: "code4craft/netty-learning"
+            }
+        },
+        'gh-pages': {
+            options: {
+                base: '.grunt/gitbook'
+            },
+            src: ['**']
+        },
+        'clean': {
+            files: '.grunt'
+        }
+    });
+
+    grunt.registerTask('publish', [
+        'gitbook',
+        'gh-pages',
+        'clean'
+    ]);
+    grunt.registerTask('default', 'gitbook');
+};
